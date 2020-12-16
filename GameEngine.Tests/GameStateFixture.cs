75c8331d2770a48ad0754d3ee6c27d2da1d3e5e0
Edin Jašarević <edinjasar14@gmail.com>
2020-12-16 09:42:14 +0100

    sharing context between test during execution

---
+++ GameEngine.Tests/GameStateFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine.Tests
+{
+    public class GameStateFixture: IDisposable
+    {
+        public GameStateFixture()
+        {
+            State = new GameState();
+        }
+
+        public GameState State { get; private set; }
+
+        public void Dispose()
+        {
+            //Cleanup
+        }
+    }
+}
---
+++ GameEngine.Tests/GameStateShould.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    public class GameStateShould: IClassFixture<GameStateFixture>
+    {
+        private readonly GameStateFixture _gameStateFixture;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public GameStateShould(GameStateFixture gameStateFixture, ITestOutputHelper outputHelper)
+        {
+            _gameStateFixture = gameStateFixture;
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void DamageAllPlayersWhenEarthquake()
+        {
+            _outputHelper.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+
+            var player1 = new PlayerCharacter();
+            var player2 = new PlayerCharacter();
+
+            _gameStateFixture.State.Players.Add(player1);
+            _gameStateFixture.State.Players.Add(player2);
+
+            var expectedHealthAfterEarthquake = player1.Health - GameState.EarthquakeDamage;
+
+            _gameStateFixture.State.Earthquake();
+            
+
+            Assert.Equal(expectedHealthAfterEarthquake, player1.Health);
+            Assert.Equal(expectedHealthAfterEarthquake, player2.Health);
+        }
+
+        [Fact]
+        public void Reset()
+        {
+            _outputHelper.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+
+            var player1 = new PlayerCharacter();
+            var player2 = new PlayerCharacter();
+
+            _gameStateFixture.State.Players.Add(player1);
+            _gameStateFixture.State.Players.Add(player2);
+
+            _gameStateFixture.State.Reset();
+
+
+            Assert.Empty(_gameStateFixture.State.Players);
+        }
+    }
+}
