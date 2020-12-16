cab16396070fef2c37be85085356896d8108bf60
Edin Jašarević <edinjasar14@gmail.com>
2020-12-16 09:08:32 +0100

    writing custome test output message

--- GameEngine.Tests/BossEnemyShould.cs
+++ GameEngine.Tests/BossEnemyShould.cs
@@ -1,13 +1,23 @@
 ﻿using Xunit;
+using Xunit.Abstractions;
 
 namespace GameEngine.Tests
 {
     public class BossEnemyShould
     {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public BossEnemyShould(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
         [Fact]
         [Trait("Category", "Boss")]
         public void HaveCorrectPower()
         {
+            _outputHelper.WriteLine("Creating Boss Enemy");
+
             BossEnemy sut = new BossEnemy();
 
             Assert.Equal(166.667, sut.TotalSpecialAttackPower, 3);
