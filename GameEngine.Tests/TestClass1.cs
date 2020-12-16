11079af08a60ab8b784d864aeae876314db730ad
Edin Jašarević <edinjasar14@gmail.com>
2020-12-16 09:53:17 +0100

    sharing context accross test class classes

---
+++ GameEngine.Tests/TestClass1.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]
+    public class TestClass1
+    {
+        private readonly GameStateFixture _gameStateFixture;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public TestClass1(GameStateFixture gameStateFixture, ITestOutputHelper outputHelper)
+        {
+            _gameStateFixture = gameStateFixture;
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            _outputHelper.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            _outputHelper.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+        }
+    }
+}
---
+++ GameEngine.Tests/TestClass2.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]
+    public class TestClass2
+    {
+        private readonly GameStateFixture _gameStateFixture;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public TestClass2(GameStateFixture gameStateFixture, ITestOutputHelper outputHelper)
+        {
+            _gameStateFixture = gameStateFixture;
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            _outputHelper.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            _outputHelper.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+        }
+    }
+}
