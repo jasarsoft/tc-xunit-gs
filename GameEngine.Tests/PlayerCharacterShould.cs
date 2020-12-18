24523df9ab540b277ea7b904849017d038039769
Edin Jašarević <edinjasar14@gmail.com>
2020-12-18 22:09:03 +0100

    finish

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -167,36 +167,16 @@ namespace GameEngine.Tests
             Assert.PropertyChanged(_sut, "Health", () => _sut.TakeDamage(10));
         }
 
-        [Fact]
-        public void TakeZeroDamage()
+        [Theory]
+        [InlineData(0, 100)]
+        [InlineData(1, 99)]
+        [InlineData(50, 50)]
+        [InlineData(101, 1)]
+        public void TakeDamage(int damage, int expectedHealth)
         {
-            _sut.TakeDamage(0);
+            _sut.TakeDamage(damage);
 
-            Assert.Equal(100, _sut.Health);
-        }
-
-        [Fact]
-        public void TakeSmallDamage()
-        {
-            _sut.TakeDamage(1);
-
-            Assert.Equal(99, _sut.Health);
-        }
-
-        [Fact]
-        public void TakeMediumDamage()
-        {
-            _sut.TakeDamage(50);
-
-            Assert.Equal(50, _sut.Health);
-        }
-
-        [Fact]
-        public void HaveMinimum1Health()
-        {
-            _sut.TakeDamage(101);
-
-            Assert.Equal(1, _sut.Health);
+            Assert.Equal(expectedHealth, _sut.Health);
         }
     }
 }
